0ae3706db84f4f5b35f91062045009d0267526ca
Mostafa Enjin <gorgmostafa16@gmail.com>
2019-01-29 12:59:56 +0330

    new Modified For Command Table

--- Mvc/Models/Entity/NewsCommand.cs
+++ Mvc/Models/Entity/NewsCommand.cs
@@ -7,5 +7,13 @@ namespace Mvc.Models.Entity
 {
     public class NewsCommand
     {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public int NewsId { get; set; }
+        public string Commandtxt { get; set; }
+        public DateTime LikeDate { get; set; }
+
+        public virtual Account Account { get; set; }
+        public virtual News News { get; set; }
     }
 }

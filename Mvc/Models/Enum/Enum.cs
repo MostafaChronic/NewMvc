fad70010b27e1fe4dc0b1b97ff2e6e4030c7f16b
Mostafa Enjin <gorgmostafa16@gmail.com>
2019-01-29 12:54:11 +0330

    add new class for enumes

---
+++ Mvc/Models/Enum/Enum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc.Models.Enum
+{
+    public class Enum
+    {
+
+        public enum AccountTypes
+        {
+            Admin,
+            User,
+            Dump
+        }
+    }
+}

fefe261b91a5cbefeb4bf14dd78a07c56ce30ab6
Mostafa Enjin <gorgmostafa16@gmail.com>
2019-01-29 12:54:37 +0330

    add new class for entityes

--- Mvc/Models/Entity/Account.cs
+++ Mvc/Models/Entity/Account.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using static Mvc.Models.Enum.Enum;
 
 namespace Mvc.Models.Entity
 {
     public class Account
     {
+
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool Gender { get; set; }
+        public bool IsBlock { get; set; }
+        public DateTime  RegisterDate { get; set; }
+
+        public AccountTypes AccountType { get; set; }
+
+        public virtual ICollection<News> Newses { get; set; }
+        public virtual ICollection<NewsLike> NewsLikes { get; set; }
+        public virtual ICollection<NewsCommand> NewsCMD { get; set; }
+
+
     }
 }
--- Mvc/Models/Entity/NewsLike.cs
+++ Mvc/Models/Entity/NewsLike.cs
@@ -7,5 +7,12 @@ namespace Mvc.Models.Entity
 {
     public class NewsLike
     {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public int NewsId { get; set; }
+        public DateTime  LikeDate { get; set; }
+
+        public virtual Account Account { get; set; }
+        public virtual News News { get; set; }
     }
 }
